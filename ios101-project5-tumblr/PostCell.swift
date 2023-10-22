8f946b8cbb914022c6d9a3cd870b267f1ea81bcd
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-10-22 11:50:37 -0400

    Setup custom TableVIew cell UI and outlets

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Paul Leiva on 10/22/23.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postCaption: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
